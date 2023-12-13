3924b01eb170b9d2c70f1e834da3340341822968
Jesús <jesusguerreropaniagua6@gmail.com>
2023-12-13 22:33:48 +0100

    Board server Action

--- components/form/form-picker.tsx
+++ components/form/form-picker.tsx
@@ -2,9 +2,15 @@
 
 import { useState, useEffect } from "react";
 import { useFormStatus } from "react-dom";
-import { Loader2 } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { Check, Loader2 } from "lucide-react";
 
+import { cn } from "@/lib/utils";
 import { unsplash } from "@/lib/unsplash";
+
+import { defaultImages } from "@/constants/images";
+import { FormErrors } from "./form-errors";
 
 interface FormPickerProps {
   id: string;
@@ -34,7 +40,7 @@ export const FormPicker = ({ id, errors }: FormPickerProps) => {
         }
       } catch (error) {
         console.error({ error });
-        setImages([]);
+        setImages(defaultImages);
       } finally {
         setIsLoading(false);
       }
@@ -51,5 +57,53 @@ export const FormPicker = ({ id, errors }: FormPickerProps) => {
     );
   }
 
-  return <div className="relative"></div>;
+  return (
+    <div className="relative">
+      <div className="grid grid-cols-3 gap-2 mb-2">
+        {images.map((image) => (
+          <div
+            key={image.id}
+            className={cn(
+              "cursor-pointer relative aspect-video group hover:opacity-75 transition bg-muted",
+              pending && "opacity-50 hover:opacity-50 cursor-auto"
+            )}
+            onClick={() => {
+              if (pending) return;
+              setSelectedImageId(image.id);
+            }}
+          >
+            <input
+              type="radio"
+              id={id}
+              name={id}
+              className="hidden"
+              checked={selectedImageId === image.id}
+              disabled={pending}
+              onChange={() => {}}
+              value={`${image.id}|${image.urls.thumb}|${image.urls.full}|${image.links.html}|${image.user.name}`}
+            />
+            <Image
+              fill
+              alt="unsplash image"
+              className="object-cover rounded-sm"
+              src={image.urls.thumb}
+            />
+            {selectedImageId === image.id && (
+              <div className="absolute inset-y-0 h-full w-full bg-black/30 flex items-center justify-center">
+                <Check className="h-4 w-4 text-white" />
+              </div>
+            )}
+            <Link
+              href={image.links.html}
+              target="_blank"
+              className="opacity-0 group-hover:opacity-100 absolute bottom-0 w-full text-[10px] truncate text-white hover:underline p-1 bg-black/10"
+            >
+              {image.user.name}
+            </Link>
+          </div>
+        ))}
+      </div>
+      <FormErrors errors={errors} id="images" />
+    </div>
+  );
 };
--- components/form/form-popover.tsx
+++ components/form/form-popover.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import { ElementRef, useRef } from "react";
 import { X } from "lucide-react";
+import { toast } from "sonner";
 
 import {
   Popover,
@@ -14,8 +16,8 @@ import { createBoard } from "@/actions/create-board";
 
 import { FormSubmit } from "./form-submit";
 import { FormInput } from "./form-input";
-import { toast } from "sonner";
 import { FormPicker } from "./form-picker";
+import { useRouter } from "next/navigation";
 
 interface FormPopoverProps {
   children: React.ReactNode;
@@ -30,21 +32,25 @@ export const FormPopover = ({
   align,
   sideOffset = 0,
 }: FormPopoverProps) => {
+  const router = useRouter();
+  const closeRef = useRef<ElementRef<"button">>(null);
+
   const { execute, fieldErrors } = useAction(createBoard, {
     onSuccess: (data) => {
-      console.log({ data });
       toast.success("Board created");
+      closeRef.current?.click();
+      router.push(`/board/${data.id}`);
     },
     onError: (error) => {
-      console.error({ error });
       toast.error(error);
     },
   });
 
   const onSubmit = (formData: FormData) => {
     const title = formData.get("title") as string;
+    const image = formData.get("image") as string;
 
-    execute({ title });
+    execute({ title, image });
   };
 
   return (
@@ -59,7 +65,7 @@ export const FormPopover = ({
         <div className="text-sm font-medium text-center text-neutral-600 pb-4">
           Create Board
         </div>
-        <PopoverClose asChild>
+        <PopoverClose ref={closeRef} asChild>
           <Button
             className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600"
             variant="ghost"
